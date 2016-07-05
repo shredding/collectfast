959ff1e6d3082e1990d962486b6ac22811d02c60
Christian Peters <chp@digitale-avantgarde.com>
2016-07-05 16:02:04 +0200

    Unicode Error in python 3 fix

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+    
 from distutils.core import setup
 from setuptools import find_packages
 
@@ -7,7 +7,7 @@ setup(
     name='Collectfast',
     description='Collectstatic on Steroids',
     version='0.2.3',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', encoding='utf-8').read(),
     author='Anton Agestam',
     author_email='msn@antonagestam.se',
     packages=find_packages(),
